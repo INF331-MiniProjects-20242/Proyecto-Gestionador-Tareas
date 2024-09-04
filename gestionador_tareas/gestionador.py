afa54c9212c67b70f97bc906163b094cadf24cb2
Pepi74 <pepigaming74@gmail.com>
2024-09-04 02:25:03 -0400

    realizado cambios en funciones
    * En cargar_tareas se recibe al usuario y se crea un diccionario indice_tareas, que guarda la tarea i del usuario con el indice correspondiente en el archivo JSON.
    * Debido al cambio anterior, se modifica el funcionamiento de mostrar_tareas, actualizar_tarea y eliminar_tarea, en donde recibe este diccionario.
    * Se crea una nueva funcion que recibe la lista de tareas y el diccionario de indices, si uno de los dos esta vacio, imprime un mensaje por pantalla y retorna False, en caso contrario True.

--- gestionador_tareas/gestionador.py
+++ gestionador_tareas/gestionador.py
@@ -9,18 +9,36 @@ logging.basicConfig(filename="app.log", filemode='a', format='%(asctime)s - %(le
 
 ARCHIVO_TAREAS = "tareas.json"
 
-def cargar_tareas():
+def verificacion_tareas_indices(tareas, indices_tareas):
+    if not tareas:
+        print("No hay tareas disponibles.")
+        return False
+    elif not indices_tareas:
+        print("No hay tareas disponibles con tu usuario.")
+        return False
+    return True
+
+def cargar_tareas(usuario):
     try:
+        tareas = []
+        indices_tareas = {}
+        j = 1
         with open(ARCHIVO_TAREAS, 'r') as file:
             tareas_data = json.load(file)
-            return [Tarea.from_dict(t) for t in tareas_data]
+            for i, t in enumerate(tareas_data):
+                tareas.append(Tarea.from_dict(t))
+                if t["usuario"] == usuario:
+                    indices_tareas[j] = i
+                    j += 1
+            return tareas, indices_tareas
+
     except FileNotFoundError as e:
         logging.warning(f"Archivo de tareas no encontrado, se creara uno nuevo al crear una nueva tarea: {e}")
         print(f"Archivo de tareas no encontrado, se creara uno nuevo al crear una nueva tarea: {e}")
-        return []
+        return [], {}
     except json.JSONDecodeError as e:
         logging.error(f"Error al leer el archivo de tareas, no existen datos en archivo JSON: {e}")
-        return []
+        return [], {}
 
 def guardar_tareas(tareas):
     try:
@@ -72,7 +90,7 @@ def crear_tarea(usuario):
         tarea = Tarea(titulo, descr, fecha, tipo, usuario=usuario)
         logging.info(f"Tarea '{tarea.titulo}' creada por el usuario {usuario}")
 
-        tareas = cargar_tareas()
+        tareas, _ = cargar_tareas(usuario)
         tareas.append(tarea)
 
         # Guardado automatico despues de crear tarea
@@ -83,94 +101,93 @@ def crear_tarea(usuario):
         print(f"Error: No se pudo crear la tarea. Verifique los datos ingresados: {e}")
         return None
     
-def mostrar_tareas(tareas):
-    if not tareas:
-        print("No hay tareas disponibles.")
-        return
-    for i, tarea in enumerate(tareas, start=1):
+def mostrar_tareas(tareas, indices_tareas):
+    for i, tarea in enumerate(indices_tareas, start=1):
         print(f"\nTarea {i}:\n")
-        tarea.VerTarea()
+        tareas[indices_tareas[tarea]].VerTarea()
 
 def actualizar_tarea(usuario):
-    tareas = cargar_tareas()
-    mostrar_tareas(tareas)
-    try:
-        seleccion = int(input("Ingrese el numero de la tarea a actualizar: "))
-        if 1 <= seleccion <= len(tareas):
-            tarea = tareas[seleccion - 1]
-            print(f"Actualizando tarea '{tarea.titulo}'")
-            # Verificacion de titulo para que no exceda los 50 caracteres
-            while True:
-                titulo = input(f"Nuevo titulo (dejar en blanco para mantener '{tarea.titulo}'): ") or tarea.titulo
-                if len(titulo) > 50:
-                    print("El titulo debe contener a lo mas 50 caracteres, intenta nuevamente")
+    tareas, indices_tareas = cargar_tareas(usuario)
+    if verificacion_tareas_indices(tareas, indices_tareas):
+        mostrar_tareas(tareas, indices_tareas)
+        try:
+            seleccion = int(input("Ingrese el numero de la tarea a actualizar: "))
+            if 1 <= seleccion <= len(indices_tareas):
+                tarea = tareas[indices_tareas[seleccion]]
+                print(f"Actualizando tarea '{tarea.titulo}'")
+                # Verificacion de titulo para que no exceda los 50 caracteres
+                while True:
+                    titulo = input(f"Nuevo titulo (dejar en blanco para mantener '{tarea.titulo}'): ") or tarea.titulo
+                    if len(titulo) > 50:
+                        print("El titulo debe contener a lo mas 50 caracteres, intenta nuevamente")
+                    else:
+                        break
+                    # Verificacion de descripcion para que no exceda los 200 caracteres
+                while True:
+                    descr = input(f"Nueva descripcion (dejar en blanco para mantener '{tarea.descr}'): ") or tarea.descr
+                    if len(descr) > 200:
+                        print("La descripcion debe contener a los mas 200 caracteres, intenta nuevamente")
+                    else:
+                        break
+                
+                # Actualizar la fecha de vencimiento si se proporciona una nueva
+                while True:
+                    fecha_str = input(f"Nueva fecha de vencimiento (YYYY-MM-DD HH:MM) o presione Enter para mantener '{tarea.fecha}'): ")
+                    # Verificacion de fecha ingresada, si el formato entregado corresponde a una fecha posterior a la actual o si cumple el formato YYYY-MM-DD
+                    if fecha_str:
+                        try:
+                            fecha_val = datetime.datetime.strptime(fecha_str, '%Y-%m-%d').date()
+                            if fecha_val < datetime.date.today():
+                                print("Fecha ingresada no puede ser anterior a la actual, intenta nuevamente")
+                            else:
+                                break
+                        except ValueError:
+                            print("Formato de fecha invalido, intenta nuevamente con el formato YYYY-MM-DD")
+                    else:
+                        break
+                fecha = datetime.datetime.strptime(fecha_str, '%Y-%m-%d') if fecha_str else tarea.fecha
+                
+                # Flag que indica si se cambia la etiqueta o no
+                tipo_f = input(f"¿Desea mantener la etiqueta (dejar en blanco) o cambiar la etiqueta (ingrese 1)?: ")
+                if tipo_f == "1":
+                    etiquetas = cargar_etiquetas()
+                    tipo = obtener_etiqueta(etiquetas)
                 else:
-                    break
-                # Verificacion de descripcion para que no exceda los 200 caracteres
-            while True:
-                descr = input(f"Nueva descripcion (dejar en blanco para mantener '{tarea.descr}'): ") or tarea.descr
-                if len(descr) > 200:
-                    print("La descripcion debe contener a los mas 200 caracteres, intenta nuevamente")
-                else:
-                    break
-            
-            # Actualizar la fecha de vencimiento si se proporciona una nueva
-            while True:
-                fecha_str = input(f"Nueva fecha de vencimiento (YYYY-MM-DD HH:MM) o presione Enter para mantener '{tarea.fecha}'): ")
-                # Verificacion de fecha ingresada, si el formato entregado corresponde a una fecha posterior a la actual o si cumple el formato YYYY-MM-DD
-                if fecha_str:
-                    try:
-                        fecha_val = datetime.datetime.strptime(fecha_str, '%Y-%m-%d').date()
-                        if fecha_val < datetime.date.today():
-                            print("Fecha ingresada no puede ser anterior a la actual, intenta nuevamente")
-                        else:
-                            break
-                    except ValueError:
-                        print("Formato de fecha invalido, intenta nuevamente con el formato YYYY-MM-DD")
-                else:
-                    break
-            fecha = datetime.datetime.strptime(fecha_str, '%Y-%m-%d') if fecha_str else tarea.fecha
-            
-            # Flag que indica si se cambia la etiqueta o no
-            tipo_f = input(f"¿Desea mantener la etiqueta (dejar en blanco) o cambiar la etiqueta (ingrese 1)?: ")
-            if tipo_f == "1":
-                etiquetas = cargar_etiquetas()
-                tipo = obtener_etiqueta(etiquetas)
-            else:
-                tipo = tarea.tipo
-            
-            # Actualizacion atributos de tarea
-            tarea.titulo = titulo
-            tarea.descr = descr
-            tarea.fecha = fecha
-            tarea.tipo = tipo
+                    tipo = tarea.tipo
+                
+                # Actualizacion atributos de tarea
+                tarea.titulo = titulo
+                tarea.descr = descr
+                tarea.fecha = fecha
+                tarea.tipo = tipo
 
-            # Guardar tarea actualizada 
-            guardar_tareas(tareas)
-            logging.info(f"Tarea '{tarea.titulo}' actualizada por el usuario {usuario}")
-            print("Tarea actualizada con exito.")
-        else:
-            print("Numero de tarea invalido.")
-    except ValueError:
-        print("Entrada invalida.")
-        logging.error("Error al actualizar la tarea: Entrada invalida.")
+                # Guardar tarea actualizada 
+                guardar_tareas(tareas)
+                logging.info(f"Tarea '{tarea.titulo}' actualizada por el usuario {usuario}")
+                print("Tarea actualizada con exito.")
+            else:
+                print("Numero de tarea invalido.")
+        except ValueError:
+            print("Entrada invalida.")
+            logging.error("Error al actualizar la tarea: Entrada invalida.")
 
 def eliminar_tarea(usuario):
-    tareas = cargar_tareas()
-    mostrar_tareas(tareas)
-    try:
-        seleccion = int(input("Ingrese el numero de la tarea a eliminar: "))
-        if 1 <= seleccion <= len(tareas):
-            tarea = tareas.pop(seleccion - 1)
-            logging.info(f"Tarea '{tarea.titulo}' eliminada por el usuario {usuario}")
+    tareas, indices_tareas = cargar_tareas(usuario)
+    if verificacion_tareas_indices(tareas, indices_tareas):
+        mostrar_tareas(tareas, indices_tareas)
+        try:
+            seleccion = int(input("Ingrese el numero de la tarea a eliminar: "))
+            if 1 <= seleccion <= len(indices_tareas):
+                tarea = tareas.pop(indices_tareas[seleccion])
+                logging.info(f"Tarea '{tarea.titulo}' eliminada por el usuario {usuario}")
 
-            guardar_tareas(tareas)  # Guardar la lista de tareas actualizada
-            print("Tarea eliminada con exito.")
-        else:
-            print("Numero de tarea invalido.")
-    except ValueError:
-        print("Entrada invalida.")
-        logging.error("Error al eliminar la tarea: Entrada invalida.")
+                guardar_tareas(tareas)  # Guardar la lista de tareas actualizada
+                print("Tarea eliminada con exito.")
+            else:
+                print("Numero de tarea invalido.")
+        except ValueError:
+            print("Entrada invalida.")
+            logging.error("Error al eliminar la tarea: Entrada invalida.")
 
 def main(cuenta):
     logging.info(f"El usuario {cuenta.usuario} ha entrado al gestionador de tareas.")
@@ -189,8 +206,9 @@ def main(cuenta):
         elif eleccion == "2":
             crear_tarea(cuenta.usuario)
         elif eleccion == "3":
-            tareas = cargar_tareas()
-            mostrar_tareas(tareas)
+            tareas, indices_tareas = cargar_tareas(cuenta.usuario)
+            if verificacion_tareas_indices(tareas, indices_tareas):
+                mostrar_tareas(tareas, indices_tareas)
         elif eleccion == "4":
             actualizar_tarea(cuenta.usuario)
         elif eleccion == "5":
