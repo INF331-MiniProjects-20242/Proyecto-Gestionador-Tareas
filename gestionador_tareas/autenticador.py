bfde581cddc3f4a15e61502a2ee43f2712e5a3ce
Pepi74 <pepigaming74@gmail.com>
2024-09-05 20:32:21 -0400

    funciones de autenticacion movidas a autenticador

--- gestionador_tareas/autenticador.py
+++ gestionador_tareas/autenticador.py
@@ -6,8 +6,6 @@ import logging
 # Configuracion de logging
 logging.basicConfig(filename="app.log", filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-
-
 def hash_contrasena(contrasena: str):
     return bcrypt.hashpw(contrasena.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
@@ -57,3 +55,82 @@ def verificar_cuenta(usuario: str, contrasena: str):
     except Exception as e:
         logging.error(f"Error al verificar cuenta: {e}")
     return False
+
+def desplegar_registro():
+    try:
+        print("\n--- Creacion de Cuenta ---")
+
+        print("Escriba su nombre: ")
+        nombre = input()
+
+        no_unico = True
+        while no_unico:
+            print("Escriba un usuario unico: ")
+            usuario = input()
+            no_unico = existe_usuario(usuario)
+
+            if no_unico:
+                #El usuario escribio un nombre ya utilizado por otro
+                print("El usuario escrito ya existe en el sistema!")
+                print("Intenta con otro porfavor!\n")
+
+        print("Escriba una contraseña: ")
+        contrasena = input()
+
+        crear_cuenta(usuario, nombre, contrasena)
+        print("La cuenta ha sido creada con exito!")
+    except Exception as e:
+        print(f"Error durante el registro: {e}")
+        logging.error(f"Error durante el despliegue registro: {e}", "Error")
+
+
+def desplegar_login():
+    try:
+        print("\n--- Login ---")
+        print("Escriba su nombre de usuario: ")
+        usuario = input()
+        print("Escriba su contraseña: ")
+        contrasena = input()
+
+        logea = verificar_cuenta(usuario, contrasena)
+
+        if logea:
+            print("\nLogin existoso!\n")
+            return True
+        else:
+            print("Usuario o contraseña equivocados, regresando al menu...")
+    except Exception as e:
+        print(f"Error durante el login: {e}")
+        logging.error(f"Error durante el despliegue del login: {e}", "Error")
+    return False
+
+def main():
+    #Aqui creo el archivo si no existe
+    archivo = "users.txt"
+    f = open(archivo, 'a')
+    f.close()
+    logging.info("Se ha entrado al autenticador de cuentas.")
+    while True: 
+        try:
+            print("\nBienvenido al autenticador de cuentas")
+            print("Seleccione accion: ")
+            print("1) Ingresar con tu Cuenta")
+            print("2) Registrar Cuenta")
+            print("3) Salir")
+            eleccion = input("Escriba el numero a seleccionar: ")
+            if eleccion == "1":
+                desplegar_login()
+            elif eleccion == "2":
+                if desplegar_registro():
+                    print("Mandar al menu de tareas")
+            elif eleccion == "3":
+                logging.info("Se ha salido del autenticador de cuentas.")
+                break
+            else:
+                print("No existe tal accion!")
+        except Exception as e:
+            print(f"Error durante el Menu de inicio: {e}")
+            logging.error(f"Error durante el Menu de inicio: {e}", "Error")
+
+if __name__ == "__main__":
+    main()
